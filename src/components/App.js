47db8c3807a41433b7e996a5eeb71ee107679749
Matias <matiascejas8@gmail.com>
2019-02-02 06:45:54 -0300

    se optimizo el codigo para poder obtener usuarios de una api externa

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,46 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import CardList from './CardList'
 import SearchBox from './SearchBox.js'
-import { robots } from './robots'
 import '../css/app.scss'
 
 class App extends Component {
-  constructor () {
+  constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
   }
-  onSearchChange = (event) => {
-    this.setState({searchfield: event.target.value})
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({
+        robots: users
+      }))
   }
-  render () {
+
+  onSearchChange = (event) => {
+    this.setState({
+      searchfield: event.target.value
+    })
+  }
+  render() {
+
     const filteredRobot = this.state.robots.filter(robot => {
       return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
     })
-    return (
-      <div className='tc'>
-        <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobot} />
-      </div>
-    )
+
+  if (this.state.robots.lenght === 0) {
+    return <h1>Loading</h1>
+  } else {
+    return ( 
+    <div className = 'tc'>
+      <h1 className = "f1">RoboFriends</h1> 
+      <SearchBox searchChange = {this.onSearchChange}/> 
+      <CardList robots = {filteredRobot}/>
+    </div>
+      )
   }
 }
-
+}
 export default App
